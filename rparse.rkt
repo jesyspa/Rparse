09df7cf83ed776820328da3dd7303f4c94d0de70
Anton Golov <jesyspa@gmail.com>
2012-06-02 00:12:33 +0200

    Finished lexer.

--- rparse.rkt
+++ rparse.rkt
@@ -9,16 +9,22 @@
 (define-empty-tokens rparse-empty-tok
   (lparen rparen comma dot at tilde))
 
+
+
 (define rparse-lexer
   (lexer
-    [(re-+ numeric) (token-number (string->number lexeme))]
-    [(re-+ symbolic) (token-symbol lexeme)]
-    [#\( (token-lparen)]
-    [#\) (token-rparen)]
-    [#\, (token-comma)]
-    [#\. (token-dot)]
-    [#\@ (token-at)]
-    [#\~ (token-tilde)]))
+   [(re-+ numeric) (token-number (string->number lexeme))]
+   [(re-: (re-- (re-or alphabetic punctuation symbolic)
+                #\( #\) #\, #\. #\@ #\~)
+          (re-* (re-or (re-- (re-or alphabetic punctuation symbolic)
+                             #\( #\) #\, #\. #\@ #\~)
+                       numeric))) (token-symbol lexeme)]
+   [#\( (token-lparen)]
+   [#\) (token-rparen)]
+   [#\, (token-comma)]
+   [#\. (token-dot)]
+   [#\@ (token-at)]
+   [#\~ (token-tilde)]))
 
 
 ;;; Tests
@@ -27,21 +33,37 @@
 
 (provide lex-tests)
 
+(define-syntax-rule (parse-check-equal? string type value)
+  (let ([result (rparse-lexer (open-input-string string))])
+    (check-equal? (token-name result) 'type)
+    (check-equal? (token-value result) value)))
+
 (define lex-tests
   (test-suite
    "Tests for the lexer."
    
    (test-case
     "Numbers are lexed correctly."
-    (let ([result (rparse-lexer (open-input-string "5"))])
-      (check-equal? (token-name result) 'number)
-      (check-equal? (token-value result) 5))
-    (let ([result (rparse-lexer (open-input-string "103"))])
-      (check-equal? (token-name result) 'number)
-      (check-equal? (token-value result) 103)))
+    (parse-check-equal? "5" number 5)
+    (parse-check-equal? "100" number 100)
+    (parse-check-equal? "0" number 0))
    
    (test-case
     "Symbols are lexed correctly."
-    (let ([result (rparse-lexer (open-input-string "x"))])
-      (check-equal? (token-name result) 'symbol)
-      (check-equal? (token-value result) "x")))))
+    (parse-check-equal? "x" symbol "x")
+    (parse-check-equal? "+" symbol "+")
+    (parse-check-equal? "-" symbol "-")
+    (parse-check-equal? "*" symbol "*")
+    (parse-check-equal? "/" symbol "/")
+    (parse-check-equal? "%" symbol "%")
+    (parse-check-equal? "?" symbol "?")
+    (parse-check-equal? "a5" symbol "a5"))
+   
+   (test-case
+    "Literals are lexed correctly."
+    (parse-check-equal? "(" lparen #f)
+    (parse-check-equal? ")" rparen #f)
+    (parse-check-equal? "," comma #f)
+    (parse-check-equal? "." dot #f)
+    (parse-check-equal? "@" at #f)
+    (parse-check-equal? "~" tilde #f))))
