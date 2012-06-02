adb2af944e088b874d58f15b75bb7ab813c108b3
Anton Golov <jesyspa@gmail.com>
2012-06-02 09:35:35 +0200

    Implemented env and reval.

---
+++ env.rkt
@@ -0,0 +1,54 @@
+#lang racket
+
+(require unstable/contract)
+
+(provide (contract-out
+          [env? (any/c . -> . boolean?)]
+          [make-env ((option/c env?) . -> . env?)]
+          [env-lookup (env? string? . -> . any/c)]
+          [env-define! (env? string? any/c . -> . any/c)]
+          [env-parent (env? . -> . (option/c env?))]))
+         
+(struct env (parent values))
+
+(define (make-env parent)
+  (env parent (make-hash)))
+
+(define (get-hash-with e str)
+  (if (hash-has-key? (env-values e) str)
+      (env-values e)
+      (if (false? (env-parent e))
+          #f
+          (get-hash-with (env-parent e) str))))
+
+(define (env-define! e str value)
+  (hash-set! (env-values e) str value))
+
+(define (env-lookup e str)
+  (hash-ref (get-hash-with e str) str))
+
+;;; Tests
+
+(require rackunit)
+
+(provide env-tests)
+
+(define env-tests
+ (test-suite
+  "Tests for the environment."
+  
+  (test-case
+   "Check that creation is possible."
+   (check-equal? (env? (make-env #f)) #t))
+  
+  (test-case
+   "Check that adding a value works."
+   (let ([e (make-env #f)])
+     (env-define! e "x" 5)
+     (check-equal? (hash-ref (env-values e) "x") 5)))
+  
+  (test-case
+   "Check that looking values up works."
+   (let ([e (make-env #f)])
+     (env-define! e "x" 5)
+     (check-equal? (env-lookup e "x") 5)))))
--- run_tests.rkt
+++ run_tests.rkt
@@ -1,6 +1,12 @@
 #lang racket
 
 (require rackunit/text-ui
-         "rparse.rkt")
+         "rparse.rkt"
+         "env.rkt"
+         "reval.rkt")
 
 (run-tests lex-tests)
+(run-tests parse-tests)
+(run-tests env-tests)
+(run-tests rtree-tests)
+(run-tests reval-tests)
